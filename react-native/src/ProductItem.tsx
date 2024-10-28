3a9dc363d221e5d5ddd00f757c7b93f3fe0d7cac
sebastian <sebastian.guhl@student.uni-luebeck.de>
2024-10-28 02:31:00 +0100

    Add card animations

--- react-native/src/ProductItem.tsx
+++ react-native/src/ProductItem.tsx
@@ -1,10 +1,11 @@
+import React, { useRef, useState } from "react";
+import { StyleSheet, View, Image, TouchableHighlight, LayoutChangeEvent, Animated } from "react-native";
 import { Text } from "react-native-paper"
-import React, { useState } from "react";
-import { StyleSheet, View, Image, TouchableHighlight } from "react-native"
 import { Inventory } from "./store/inventory";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-function isProductNewByDate(dateToCheck: Date): boolean {
+// Checks, if a product was added in the last seven days
+function isNewProductByDate(dateToCheck: Date): boolean {
     // Get the current date
     const currentDate = new Date(); 
     const sevenDaysAgo = new Date(currentDate);
@@ -18,6 +19,7 @@ function isProductNewByDate(dateToCheck: Date): boolean {
 export default React.memo(({ inventory }: { inventory: Inventory }) => {
     // Hook to store if the Card is expanded
     const [expanded, setExpanded] = useState(false);
+    const [titleTruncated, setTitleTruncated] = useState(true)
 
     // Get the images aspect ratio and store it in the hook
     const [imageAspectRatio, setImageAspectRatio] = useState(1);
@@ -25,18 +27,61 @@ export default React.memo(({ inventory }: { inventory: Inventory }) => {
 
     // Convert category array to a string
     const categories = inventory.fields["Product Categories"]?.split(", ")
+
+    // The height of the categories container
+    const [categoriesHeight, setCategoriesHeight] = useState(0)
+    
+    // This value is animated between 0 and 1
+    const animatedValue = useRef(new Animated.Value(0)).current;
+
+    // Expands/Collapses the card
+    const toggleCard = () => {
+        if(expanded) {
+            Animated.timing(animatedValue, {
+                toValue: 0,
+                duration: 500,
+                useNativeDriver: false,
+              }).start(() => {
+                // This callback runs when the animation completes
+                setTitleTruncated(!titleTruncated)
+              });
+        } else {
+            setTitleTruncated(!titleTruncated)
+            Animated.timing(animatedValue, {
+                toValue: 1,
+                duration: 500,
+                useNativeDriver: false,
+              }).start();
+        }
+        setExpanded(!expanded)
+    };
+
+    // Dynamically get the height of the categories container
+    const CategoriesLayout = (event: LayoutChangeEvent) => {
+        const layoutHeight = event.nativeEvent.layout.height
+
+        if (layoutHeight > 0 && layoutHeight !== categoriesHeight) {
+            setCategoriesHeight(layoutHeight)
+        }
+    }
     
     return (
         // Touchable component to handle press event
-        <TouchableHighlight style={styles.touchable} onPress={() => setExpanded(!expanded)} underlayColor="gray">
+        <TouchableHighlight style={styles.touchable} onPress={toggleCard} underlayColor="gray">
             <View style={styles.cardContainer}>
                 {/* This container contains either the product image or a fallback icon */}
                 <View style = {styles.imageContainer}>
                     {inventory.fields["Product Image"] ? (
-                        <Image 
+                        <Animated.Image 
                             source={{ uri: inventory.fields["Product Image"] }}
                             // dynamically set the aspect ratio
-                            style={[styles.image, {aspectRatio:expanded ? imageAspectRatio : 1}]}
+                            style={[
+                                styles.image, 
+                                {aspectRatio: animatedValue.interpolate({
+                                    inputRange: [0, 1],
+                                    outputRange: [1, imageAspectRatio]
+                                })}
+                            ]}
                         />
                     ) : (
                         <MaterialCommunityIcons
@@ -53,9 +98,20 @@ export default React.memo(({ inventory }: { inventory: Inventory }) => {
                     <View style={styles.titleDateContainer}>
                         {/* The title row contains the title of the card, possibly a new icon and the expand/collapse button */}
                         <View style={styles.titleRow}>
-                            <Text numberOfLines={expanded ? undefined : 1} style={styles.title}>{inventory.fields["Product Name"]}</Text>
+                            <Animated.View 
+                                style={{
+                                    flexShrink: 1,
+                                    maxHeight: animatedValue.interpolate({
+                                        inputRange: [0, 1],
+                                        outputRange: [24, 240]
+                                    })
+                                }}
+                                >
+                                    <Text numberOfLines={titleTruncated ? 1 : 10} style={styles.title}>{inventory.fields["Product Name"]}</Text>
+                            </Animated.View>
+                            
                             <View style={styles.headerIcons}>
-                                {isProductNewByDate(new Date(inventory.fields.Posted)) &&
+                                {isNewProductByDate(new Date(inventory.fields.Posted)) &&
                                     <MaterialCommunityIcons
                                         name="new-box"
                                         size={24}
@@ -75,13 +131,22 @@ export default React.memo(({ inventory }: { inventory: Inventory }) => {
                     </View>
 
                     {/* The container containing the category tags */}
-                    <View style={styles.categoryContainer}>
-                        {expanded && categories?.map((category, index) => (
-                            <View style={styles.category} key={index}>
-                                <Text numberOfLines={1} style={styles.categoryText}>{category}</Text>
-                            </View>
-                        ))}
-                    </View>
+                    <Animated.View 
+                        style={{
+                            overflow: 'hidden',
+                            height: animatedValue.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [0, categoriesHeight]
+                            })
+                        }}>
+                        <View style={styles.categoryContainer} onLayout={CategoriesLayout}>
+                            {categories?.map((category, index) => (
+                                <View style={styles.category} key={index}>
+                                    <Text numberOfLines={1} style={styles.categoryText}>{category}</Text>
+                                </View>
+                            ))}
+                        </View>
+                    </Animated.View>
                 </View>
             </View>
         </TouchableHighlight>
@@ -136,7 +201,7 @@ const styles = StyleSheet.create({
         lineHeight: 22,
         textAlign: 'left',
         flexShrink: 1,
-        marginRight: 12
+        marginRight: 12,
     },
     headerIcons: {
         flexDirection: 'row',
@@ -153,7 +218,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flexWrap: 'wrap',
         columnGap: 6,
-        rowGap:4
+        rowGap: 4,
+        position: 'absolute'
     },
     category: {
         backgroundColor: '#D4E5FF',
